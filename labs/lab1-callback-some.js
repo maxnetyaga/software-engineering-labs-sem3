5609eb866f8245e8a081a94ee2f8c6252b097ec1
Max Netyaga <max.netyaga@gmail.com>
2024-12-24 00:04:11 +0200

    Implemented asyncSeqSome. Parallel execution doesn	 work because they share id?

---
+++ labs/lab1-callback-some.js
@@ -0,0 +1,52 @@
+const wrapAsync =
+    (fn) =>
+    (...args) =>
+        setTimeout(() => fn(...args), Math.floor(Math.random() * 1000));
+
+// ######################################################################### //
+
+Array.prototype.asyncSequentialSome = function (predicate, cb) {
+    console.log("Inside asyncSome");
+
+    id = 0;
+
+    const next = () => {
+        if (id < this.length) {
+            predicate(this[id], (err, flag) => {
+                if (err) cb(err);
+
+                if (flag) {
+                    cb(null, true);
+                } else {
+                    console.log(id);
+                    id += 1;
+                    next();
+                }
+            });
+        } else cb(null, false);
+    };
+
+    next();
+};
+
+// Examples
+const x = [1, 2, 3, 4, 5];
+
+const asyncImpossiblePredicate = wrapAsync((data, cb) => cb(null, data >= 10));
+const asyncPossiblePredicate = wrapAsync((data, cb) => cb(null, data == 2));
+
+x.asyncSequentialSome(asyncImpossiblePredicate, (err, result) => {
+    console.log(
+        "asyncSequentialSome | asyncImpossiblePredicate: ",
+        "error: " + err,
+        "result: " + result
+    );
+});
+
+x.asyncSequentialSome(asyncPossiblePredicate, (err, result) => {
+    console.log(
+        "asyncSequentialSome | asyncPossiblePredicate ",
+        "error: " + err,
+        "result: " + result
+    );
+});
