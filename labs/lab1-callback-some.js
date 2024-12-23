1eb78e2f00ad53b8a2560cc1ccaf0681a7be883a
Max Netyaga <max.netyaga@gmail.com>
2024-12-24 01:25:57 +0200

    added paralel implementation

--- labs/lab1-callback-some.js
+++ labs/lab1-callback-some.js
@@ -3,7 +3,7 @@
 const wrapAsync =
     (fn) =>
     (...args) =>
-        setTimeout(() => fn(...args), Math.floor(Math.random() * 1000));
+        setTimeout(() => fn(...args), Math.floor(Math.random() * 500));
 
 const measureExecutionTime = (fn, label, cb) => {
     const start = performance.now();
@@ -42,6 +42,37 @@ const asyncSequentialSome = function (predicate, cb) {
     next();
 };
 
+const asyncParallelSome = function (predicate, cb) {
+    let completed = 0;
+    const errors = [];
+    let globalFlag = false;
+
+    const all = () => {
+        this.forEach((x) =>
+            predicate(x, (err, flag) => {
+                completed += 1;
+
+                if (err) {
+                    errors.push(err);
+                }
+
+                if (flag) globalFlag = flag;
+
+                if (
+                    (completed === this.length && !globalFlag) ||
+                    (globalFlag && flag)
+                ) {
+                    if (errors.length !== 0) {
+                        cb(new AggregateError(errors), null);
+                    } else cb(null, flag);
+                }
+            })
+        );
+    };
+
+    all();
+};
+
 // Examples
 const x = [1, 2, 3, 4, 5];
 
@@ -51,23 +82,51 @@ const asyncPossiblePredicate = wrapAsync((data, cb) => cb(null, data == 2));
 measureExecutionTime(
     asyncSequentialSome.bind(x, asyncImpossiblePredicate),
     "asyncSequentialSome | asyncImpossiblePredicate",
-    (err, result) =>
+    (err, result) => {
         console.log(
             "asyncSequentialSome | asyncImpossiblePredicate: ",
             "error: " + err,
             " # ",
-            "result: " + result
-        )
-);
-
-measureExecutionTime(
-    asyncSequentialSome.bind(x, asyncPossiblePredicate),
-    "asyncSequentialSome | asyncPossiblePredicate",
-    (err, result) =>
-        console.log(
-            "asyncSequentialSome | asyncPossiblePredicate: ",
-            "error: " + err,
-            " # ",
-            "result: " + result
-        )
+            "result: " + result,
+            "\n"
+        );
+        measureExecutionTime(
+            asyncSequentialSome.bind(x, asyncPossiblePredicate),
+            "asyncSequentialSome | asyncPossiblePredicate",
+            (err, result) => {
+                console.log(
+                    "asyncSequentialSome | asyncPossiblePredicate: ",
+                    "error: " + err,
+                    " # ",
+                    "result: " + result,
+                    "\n"
+                );
+                measureExecutionTime(
+                    asyncParallelSome.bind(x, asyncImpossiblePredicate),
+                    "asyncParallelSome | asyncImpossiblePredicate",
+                    (err, result) => {
+                        console.log(
+                            "asyncParallelSome | asyncImpossiblePredicate: ",
+                            "error: " + err,
+                            " # ",
+                            "result: " + result,
+                            "\n"
+                        );
+                        measureExecutionTime(
+                            asyncParallelSome.bind(x, asyncPossiblePredicate),
+                            "asyncParallelSome | asyncPossiblePredicate",
+                            (err, result) =>
+                                console.log(
+                                    "asyncParallelSome | asyncPossiblePredicate: ",
+                                    "error: " + err,
+                                    " # ",
+                                    "result: " + result,
+                                    "\n"
+                                )
+                        );
+                    }
+                );
+            }
+        );
+    }
 );
